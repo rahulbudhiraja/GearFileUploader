eeb2cf6d1593ca050ee20a56a6d6548efbd5ba31
Rahul <r.budhi@partner.samsung.com>
2013-10-26 18:40:34 -0700

    Initial Commit

---
+++ GearFileUploader/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GearFileUploader
+//
+//  Created by Rahul Buddhiraja - Intern on 10/26/13.
+//  Copyright (c) 2013 Rahul. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ GearFileUploader/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  GearFileUploader
+//
+//  Created by Rahul Buddhiraja - Intern on 10/26/13.
+//  Copyright (c) 2013 Rahul. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
